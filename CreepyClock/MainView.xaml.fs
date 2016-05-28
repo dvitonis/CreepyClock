07afe56abf3322d27dfb679382197647702c57ff
Darius Vitonis <darius.vitonis@gmail.com>
2016-05-28 20:39:53 +0200

    Show time

--- CreepyClock/MainView.xaml.fs
+++ CreepyClock/MainView.xaml.fs
@@ -1,11 +1,15 @@
 ﻿namespace Views
 
 open FsXaml
+open System
 
 type MainViewBase = XAML<"MainView.xaml">
 
 type MainView() =
     inherit MainViewBase()
-
+    
     override this.OnInitialize() =
-        ignore ()
+        this.Loaded.Add this.OnLoaded
+        
+    member this.OnLoaded _ =
+        this.ClockLabel.Content <- DateTime.Now.ToString("HH:mm:ss")
