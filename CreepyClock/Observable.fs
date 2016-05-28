09c86cc8608972f4ed6f2dad6adb6784f0805abf
Darius Vitonis <darius.vitonis@gmail.com>
2016-05-28 20:58:04 +0200

    Add Observable and RateLimitAgent

---
+++ CreepyClock/Observable.fs
@@ -0,0 +1,20 @@
+﻿module CreepyClock.Observable
+
+open System
+open RateLimitAgent
+
+let private observable f = 
+    { new IObservable<_> with
+        member x.Subscribe(obs) = f obs }
+  
+/// Limits the rate of emitted messages to at most one per the specified number of milliseconds
+let limitRate milliseconds (source:IObservable<_>) = 
+    observable (fun obs ->
+        let rate = RateLimitAgent(milliseconds)
+        rate.EventOccurred.Add(obs.OnNext)
+        rate.ErrorOccurred.Add(obs.OnError)
+        { new IObserver<_> with
+            member x.OnCompleted() = obs.OnCompleted()
+            member x.OnError(e) = obs.OnError(e)
+            member x.OnNext(v) = rate.AddEvent(v) }
+        |> source.Subscribe )
---
+++ CreepyClock/RateLimitAgent.fs
@@ -0,0 +1,22 @@
+﻿module CreepyClock.RateLimitAgent
+
+open System
+
+type RateLimitAgent<'T>(milliseconds:int) = 
+  let event = Event<'T>()
+  let error = Event<_>()
+  let agent = MailboxProcessor.Start(fun inbox -> 
+    let rec loop (lastMessageTime:DateTime) = async {
+      let! e = inbox.Receive()
+      let now = DateTime.UtcNow
+      if (now - lastMessageTime).TotalMilliseconds > float milliseconds then
+        try event.Trigger(e)
+        with e -> error.Trigger(e)
+        return! loop now
+      else 
+        return! loop lastMessageTime }
+    loop DateTime.MinValue )
+
+  member x.EventOccurred = event.Publish
+  member x.AddEvent(event) = agent.Post(event)
+  member x.ErrorOccurred = Event.merge agent.Error error.Publish
